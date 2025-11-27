9a63a0548b26a4fab9cecb0ade7b4d016a8a8f31
Anamul Hoque Shourov <hoqueanamul1998@gmail.com>
2025-11-27 19:06:21 +0600

    [problem solving] - [leetcode-977: Squares of a Sorted Array] (optimized)

--- leetcode_977/solution.go
+++ leetcode_977/solution.go
@@ -2,9 +2,10 @@ package leetcode_977
 
 import (
 	"fmt"
-	"sort"
+	"math"
 )
 
+/*
 func sortedSquares(nums []int) []int {
 	var newArray []int
 	for _, num := range nums {
@@ -14,9 +15,26 @@ func sortedSquares(nums []int) []int {
 	sort.Ints(newArray)
 	return newArray
 }
+*/
+
+func sortedSquares(nums []int) []int {
+	newArray := make([]int, len(nums))
+	left := 0
+	right := len(nums) - 1
+	for i := len(newArray) - 1; i >= 0; i-- {
+		if int(math.Abs(float64(nums[left]))) > int(math.Abs(float64(nums[right]))) {
+			newArray[i] = nums[left] * nums[left]
+			left++
+		} else {
+			newArray[i] = nums[right] * nums[right]
+			right--
+		}
+	}
+	return newArray
+}
 
 func TestCode() {
-	x := []int{7, -1, -2, 3}
+	x := []int{-7, -3, 2, 3, 11}
 	result := sortedSquares(x)
 	fmt.Println(result)
 }
