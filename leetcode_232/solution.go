78c5635ae24eb1d32d7608ba3dd8b913c7c39345
Anamul Hoque Shourov <hoqueanamul1998@gmail.com>
2025-12-03 19:08:09 +0600

    [problem solving] - [leetcode-232: Implement Queue using Stacks] (updated)

--- leetcode_232/solution.go
+++ leetcode_232/solution.go
@@ -2,95 +2,95 @@ package leetcode_232
 
 import "fmt"
 
-type MyQueue struct {
+type myQueue struct {
 	in  []int
 	out []int
 }
 
-func Constructor() MyQueue {
-	return MyQueue{
+func constructor() myQueue {
+	return myQueue{
 		in:  []int{},
 		out: []int{},
 	}
 }
 
-func (this *MyQueue) Push(x int) {
-	this.in = append(this.in, x)
+func (m *myQueue) push(x int) {
+	m.in = append(m.in, x)
 }
 
-func (this *MyQueue) Pop() int {
-	this.Move()
-	element := this.out[len(this.out)-1]
-	this.out = this.out[:len(this.out)-1]
+func (m *myQueue) pop() int {
+	m.move()
+	element := m.out[len(m.out)-1]
+	m.out = m.out[:len(m.out)-1]
 	return element
 }
 
-func (this *MyQueue) Peek() int {
-	this.Move()
-	if len(this.out) == 0 {
+func (m *myQueue) peek() int {
+	m.move()
+	if len(m.out) == 0 {
 		return 0
 	}
-	return this.out[len(this.out)-1]
+	return m.out[len(m.out)-1]
 }
 
-func (this *MyQueue) Empty() bool {
-	if len(this.out) == 0 && len(this.in) == 0 {
+func (m *myQueue) empty() bool {
+	if len(m.out) == 0 && len(m.in) == 0 {
 		return true
 	}
 	return false
 }
 
-func (this *MyQueue) Move() {
-	if len(this.out) == 0 {
-		for len(this.in) > 0 {
-			element := this.in[len(this.in)-1]
-			this.in = this.in[:len(this.in)-1]
-			this.out = append(this.out, element)
+func (m *myQueue) move() {
+	if len(m.out) == 0 {
+		for len(m.in) > 0 {
+			element := m.in[len(m.in)-1]
+			m.in = m.in[:len(m.in)-1]
+			m.out = append(m.out, element)
 		}
 	}
 }
 
 func TestCode() {
-	queue := Constructor()
+	queue := constructor()
 
-	queue.Push(10)
-	queue.Push(17)
-	queue.Push(22)
+	queue.push(10)
+	queue.push(17)
+	queue.push(22)
 
 	fmt.Println(queue.in)
 
-	pop1 := queue.Pop()
+	pop1 := queue.pop()
 	fmt.Println(pop1)
 
-	peek1 := queue.Peek()
+	peek1 := queue.peek()
 	fmt.Println(peek1)
 
-	empty1 := queue.Empty()
+	empty1 := queue.empty()
 	fmt.Println(empty1)
 
 	//****************************
 
 	fmt.Println(queue.out)
 
-	pop2 := queue.Pop()
+	pop2 := queue.pop()
 	fmt.Println(pop2)
 
-	peek2 := queue.Peek()
+	peek2 := queue.peek()
 	fmt.Println(peek2)
 
-	empty2 := queue.Empty()
+	empty2 := queue.empty()
 	fmt.Println(empty2)
 
 	//*****************************
 
 	fmt.Println(queue.out)
 
-	pop3 := queue.Pop()
+	pop3 := queue.pop()
 	fmt.Println(pop3)
 
-	peek3 := queue.Peek()
+	peek3 := queue.peek()
 	fmt.Println(peek3)
 
-	empty3 := queue.Empty()
+	empty3 := queue.empty()
 	fmt.Println(empty3)
 }
